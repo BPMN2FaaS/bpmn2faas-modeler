17c8825c60f6ff5dfff75e06fcbfbf111c53c402
TYueksel <st111539@stud.uni-stuttgart.de>
2022-06-20 02:04:50 +0200

    Added examples

---
+++ examples/hello-user-example/input/HelloUser/businessCode1.py
@@ -0,0 +1,5 @@
+
+
+def print_hello_user(name):
+    print(f'Hi, {name}')
+
---
+++ examples/online-shop-example/input/OnlineShop/businessCode1.py
@@ -0,0 +1,10 @@
+
+
+def extract_items(order):
+    return order.split(',')
+
+
+def check_inventory(inventories):
+    # put code here
+    return
+
